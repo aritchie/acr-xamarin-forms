1adf79a415334fbe418105a39a9f142731ed747b
Allan Ritchie <allan.ritchie@gmail.com>
2014-07-28 21:19:25 -0400

    Missed files

---
+++ Acr.XamForms.Mobile/Media/CameraOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Acr.XamForms.Mobile.Media {
+    
+    public class CameraOptions : AbstractOptions {
+        public CameraDevice Camera { get; set; }
+
+
+        public CameraOptions() {
+            this.Camera = CameraDevice.Rear;
+        }
+    }
+}
