c5557ceac655a7cc8c1dfd5511640571ce8293f0
Allan Ritchie <allan.ritchie@gmail.com>
2014-08-02 13:38:26 -0400

    Extension methods for file creation

---
+++ Acr.XamForms.Mobile/IO/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Acr.XamForms.Mobile.IO {
+    
+    public static class Extensions {
+
+        public static IFile GetFile(this IDirectory directory, string fileName) {
+            return directory
+                .Files
+                .FirstOrDefault(x => x.Name.Equals(fileName, StringComparison.Ordinal));
+        }
+
+
+        public static IFile GetOrCreateFile(this IDirectory directory, string fileName) {
+            return directory.GetFile(fileName) ?? directory.CreateFile(fileName);
+        }
+    }
+}
