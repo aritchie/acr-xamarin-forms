a78c3f2163a37646bd3a550238b4d49a47448aad
Allan Ritchie <allan.ritchie@gmail.com>
2014-08-08 15:10:23 -0400

    More extension method updates

--- Acr.XamForms.Mobile/IO/Extensions.cs
+++ Acr.XamForms.Mobile/IO/Extensions.cs
@@ -1,20 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 
 namespace Acr.XamForms.Mobile.IO {
     
     public static class Extensions {
 
+
+        /// <summary>
+        /// This will return a file location whether or not it exists
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public static IFile GetFile(this IDirectory directory, string fileName) {
+            return directory.GetExistingFile(fileName) ?? directory.CreateFile(fileName);
+        }
+
+
+        public static IFile GetExistingFile(this IDirectory directory, string fileName) {
             return directory
                 .Files
                 .FirstOrDefault(x => x.Name.Equals(fileName, StringComparison.Ordinal));
         }
 
 
-        public static IFile GetOrCreateFile(this IDirectory directory, string fileName) {
-            return directory.GetFile(fileName) ?? directory.CreateFile(fileName);
+        public static void DeleteIfExists(this IFile file) {
+            if (file.Exists)
+                file.Delete();
+        }
+
+
+        public static Task<Stream> OpenWriteAsync(this IFile file) {
+            return Task<Stream>.Factory.StartNew(file.OpenWrite);
+        }
+
+
+        public static Task<Stream> OpenReadAsync(this IFile file) {
+            return Task<Stream>.Factory.StartNew(file.OpenRead);
         }
     }
 }
