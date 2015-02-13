f7f266e30a8f26b45602b5f6a2ccb078174f4ff1
Allan Ritchie <allan.ritchie@gmail.com>
2015-02-12 20:49:42 -0500

    Update to xf 1.3.3

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 del *.nupkg
-nuget pack Acr.XamForms.nuspec
+rem nuget pack Acr.XamForms.nuspec
 nuget pack Acr.XamForms.Mobile.nuspec
 nuget pack Acr.XamForms.BarCodeScanner.nuspec
 nuget pack Acr.XamForms.UserDialogs.nuspec
